3ce95acaa83ac6fda9b5e5e2676a05ecc68f146c
Aditya Pandey <anshu61796@gmail.com>
2024-04-09 15:01:14 +0530

    Initial Commit

---
+++ BlackCoffer/BlackCofferApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackCofferApp.swift
+//  BlackCoffer
+//
+//  Created by Aditya Pandey on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BlackCofferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
