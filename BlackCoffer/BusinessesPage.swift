388108634e9caf20ac2f80c9383959be8185a689
Aditya Pandey <anshu61796@gmail.com>
2024-04-14 23:21:14 +0530

    Added two pages

---
+++ BlackCoffer/BusinessesPage.swift
@@ -0,0 +1,18 @@
+//
+//  BusinessesPage.swift
+//  BlackCoffer
+//
+//  Created by Aditya Pandey on 14/04/24.
+//
+
+import SwiftUI
+
+struct BusinessesPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BusinessesPage()
+}
